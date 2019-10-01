7021ccb1090f6a7715810081f73be7ccf9833a5e
akiyoshi0328 <akiyoshi.takahashi.50@gmail.com>
2019-10-01 13:01:18 +0900

    Add Topoc-C

--- ex01.rb
+++ ex01.rb
@@ -14,10 +14,17 @@ names(sales)
 def addition(sales)
   sum=0
   sales.each do |a,b|
-    sum =sum+b
+    sum = sum + b
   end
   puts sum
 end
 
 addition(sales)
 
+def quetion4(sales)
+
+  puts sales.count{|k,v| v <= 300 }
+
+end
+
+quetion4(sales)
